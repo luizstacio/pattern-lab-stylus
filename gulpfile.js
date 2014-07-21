d040ba76cad574015dd8264920f5c67eaf877468
luizstacio <luizstacio@gmail.com>
2014-07-21 02:24:19 -0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var clean = require('gulp-clean');
+
+var filesStylus = ['stylus/**/*.styl'];
+
+gulp.task('stylus', function () {
+
+  gulp.watch(filesStylus, function(e){
+    var fileName = e.path.slice(e.path.lastIndexOf('/')),
+        pathFile = e.path.replace(fileName, ''),
+        pathSearch = pathFile + '/**/*.styl',
+        otherFiles = pathFile + '/**/!(*.styl)',
+        pathDest = pathFile.replace('/stylus/', '/assets/');
+      
+    gulp.src(otherFiles)
+      .pipe(gulp.dest(pathDest));
+
+    gulp.src(pathSearch)
+      .pipe(stylus({
+        use: [nib()],
+        set:['compress'],
+        import: ['nib']
+      }))
+      .pipe(gulp.dest(pathDest));
+  });
+
+});
+
+// gulp.task('compile', function () {
+
+//   var pathSearch = './**/*.styl',
+//       otherFiles = './stylus/**/!(*.styl)',
+//       pathDest = './assets';
+
+//   // gulp.src(otherFiles)
+//   //   .pipe(gulp.dest(pathDest));
+
+//   gulp.src(filesStylus)
+//     .pipe(stylus())
+//     .pipe(gulp.dest('./assets'));
+// });
+
+gulp.task('default', ['stylus']);
